ef94359ebe862cc45e65c5f500e03ea8ffc91f2b
Sebastian Geidies <geidies@gmail.com>
2012-07-12 01:36:07 +0200

    misplaced ampersand

--- migrate_one_day.sh
+++ migrate_one_day.sh
@@ -13,9 +13,9 @@ ssh $SERVER 'ps auxw | grep migrate.r[b] || /opt/software/hbase-scripts/server.s
 echo "started server, sleeping 2.."
 sleep 2
 
-( 
+(
 for HOUR in $( seq 23 -1 0 ); do
     DATE=$( date -d "$( printf '2012%02d%02d %02d:00:00' $MONTH $DAY $HOUR )" "+%s" )
     $( dirname $0 )/../hbase org.jruby.Main migrate.rb -m client -t $DATE -i 3599 -s $SERVER -p 2000 >>/var/log/hbase/migrate_${MONTH}_${DAY}_${HOUR}.log
 done
-& ) 
+) &
